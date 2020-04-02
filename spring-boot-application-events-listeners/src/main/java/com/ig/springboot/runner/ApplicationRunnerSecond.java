66a9858f5cdc7ba358b6f4510d4180414172d83a
big <igbyun86@gmail.com>
2020-04-02 13:14:08 +0900

    initialize ApplicationRunner

---
+++ spring-boot-application-events-listeners/src/main/java/com/ig/springboot/runner/ApplicationRunnerSecond.java
@@ -0,0 +1,15 @@
+package com.ig.springboot.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(1)
+public class ApplicationRunnerSecond implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("===== Application Second Runner =====");
+    }
+}
