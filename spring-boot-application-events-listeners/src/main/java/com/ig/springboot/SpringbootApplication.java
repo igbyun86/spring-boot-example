1e31fca0b8fa5304e42c5fd94cd76e0ca2b36d09
big <igbyun86@gmail.com>
2020-04-02 13:16:40 +0900

    add SpringApplicationlister listener

--- spring-boot-application-events-listeners/src/main/java/com/ig/springboot/SpringbootApplication.java
+++ spring-boot-application-events-listeners/src/main/java/com/ig/springboot/SpringbootApplication.java
@@ -1,13 +1,29 @@
 package com.ig.springboot;
 
+import com.ig.springboot.listener.CustomApplicationContextInitializedEventListener;
+import com.ig.springboot.listener.CustomApplicationFailedEventListener;
+import com.ig.springboot.listener.CustomApplicationStartedEventListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
-    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication app = new SpringApplication(SpringbootApplication.class);
 
+        /**
+         * add Application listener event
+         */
+        CustomApplicationContextInitializedEventListener customApplicationContextInitializedEventListener = new CustomApplicationContextInitializedEventListener();
+        CustomApplicationStartedEventListener customApplicationStartedEventListener = new CustomApplicationStartedEventListener();
+        CustomApplicationFailedEventListener customApplicationFailedEventListener = new CustomApplicationFailedEventListener();
+        app.addListeners(
+                customApplicationContextInitializedEventListener,
+                customApplicationStartedEventListener,
+                customApplicationFailedEventListener);
+
+        app.run(args);
+
+    }
 }
