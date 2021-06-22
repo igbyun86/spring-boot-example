735aa787e6bc22617c57b069571b1ab5146490d3
big <igbyun86@gmail.com>
2021-06-22 23:23:43 +0900

    Add 외부설정

--- spring-boot-config/src/main/java/com/springboot/config/SampleRunner.java
+++ spring-boot-config/src/main/java/com/springboot/config/SampleRunner.java
@@ -1,5 +1,6 @@
 package com.springboot.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -7,18 +8,24 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SampleRunner implements ApplicationRunner {
+/*
 
     @Value("${ig.fullName}")
     private String name;
 
     @Value("${ig.age}")
     private int age;
+*/
+
+    @Autowired
+    IgProperties igProperties;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("==================");
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println(igProperties.getName());
+        System.out.println(igProperties.getAge());
+        System.out.println(igProperties.getSessionTimeout());
         System.out.println("==================");
     }
 }
