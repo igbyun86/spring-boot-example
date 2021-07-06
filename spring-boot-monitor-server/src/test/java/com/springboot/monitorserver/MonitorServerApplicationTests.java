239b976f2318ee010fa9fcf3ff361c954b0d57ad
big <igbyun86@gmail.com>
2021-07-06 09:01:39 +0900

    Add spring-boot-admin-server example project

---
+++ spring-boot-monitor-server/src/test/java/com/springboot/monitorserver/MonitorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.monitorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
